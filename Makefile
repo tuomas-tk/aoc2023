6f04196ef57f0ce2468f260ebb6d6a55272b75df
Tuomas Karjalainen <11463784+tuomas-tk@users.noreply.github.com>
2023-12-01 19:23:45 +0200

    looping hello world, improve makefile

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 default:
 	rm -rf hello.linked.elf
 	rm -rf hello.elf
-	riscv64-unknown-elf-as hello.s -g -o hello.elf -al
+	riscv64-unknown-elf-as hello.s -o hello.elf
 	riscv64-unknown-elf-ld -T qemu-riscv64-virt.ld -o hello.linked.elf hello.elf
 
 # riscv64-linux-gnu-as hello2.s -o hello.o
@@ -11,5 +11,18 @@ devicetree:
 	qemu-system-riscv64 -machine virt -bios none -machine dumpdtb=riscv64-virt.dtb
 	dtc -I dtb -O dts -o riscv64-virt.dts riscv64-virt.dtb
 
+disas:
+	hd hello.linked.elf
+	riscv64-unknown-elf-objdump -dsx hello.linked.elf
+
+# Run first `make gui` and there memsave 0x80000000 1000 memory
+disas-raw:
+	hd memory
+	riscv64-unknown-elf-objdump -D memory -b binary -m riscv --adjust-vma=0x80000000
+
 run:
 	qemu-system-riscv64 -machine virt -bios none -kernel hello.linked.elf -nographic
+# exit with CTRL+A, X
+
+gui:
+	qemu-system-riscv64 -machine virt -bios none -kernel hello.linked.elf
--- hello.s
+++ hello.s
@@ -4,19 +4,36 @@
 .equ UART_BASE_ADDR, 0x10000000
 
 _start:
-    li s1, UART_BASE_ADDR # s1 := 0x1000_0000
-    la s2, message    # s2 := <message>
-    addi s3, s2, 14   # s3 := s2 + 14
-1:
-    lb s4, 0(s2)      # s4 := (s2)
-    sb s4, 0(s1)      # (s1) := s4
-    addi s2, s2, 1    # s2 := s2 + 1
-    blt s2, s3, 1b    # if s2 < s3, branch back to 1
+  # set s1 to the output address
+  li s1, UART_BASE_ADDR # s1 := 0x1000_0000
+  # set s2 to the input address
+  la s2, message    # s2 := <message>
+  # add to s3 the length of message
+  # addi s3, s2, 14   # s3 := s2 + 14
+loop:
+  # load byte to s4 from the address in s2 (the input address)
+  lb s3, 0(s2)      # s4 := (s2)
+  # store byte from s4 to the address in s1 (the output address)
+  sb s3, 0(s1)      # (s1) := s4
+  # add one to the input address
+  addi s2, s2, 1    # s2 := s2 + 1
+  # loop back if s2<s3 (literally in the compiled code loop is replaced with -12 = 3*4 bytes = 3 instructions back)
+  bne s3, x0, loop    # if s2 < s3, branch back to 1
+  la s2, message
 
-		addi a7, zero, 93
-		addi a0, zero, 13
-		ecall
+  addi s4, x0, 0
+  # addi s4, x0, 0
+  lui s5, 0x3ffff
+sleep:
+  addi s4, s4, 1
+  blt s4, s5, sleep
 
-# .section .data
+  j loop
+
+  li a7, 93
+  li a0, 13
+  ecall
+
+# .section .rodata
 message:
   .string "Hello, world!\n"
