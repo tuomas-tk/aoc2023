6522749f5b95e5b18847adae557572b2ba09e97b
Tuomas Karjalainen <11463784+tuomas-tk@users.noreply.github.com>
2023-12-02 19:16:04 +0200

    day 1 cleanup

--- hello.s
+++ hello.s
@@ -2,7 +2,7 @@
 
 # Compile-time constants
 .equ UART_BASE_ADDR, 0x10000000
-# QEMU Test Finisher interface
+# QEMU Test Finisher interface for gracefully halting the machine
 # https://github.com/qemu/qemu/blob/master/include/hw/misc/sifive_test.h
 .equ VIRT_TEST, 0x100000
 .equ FINISHER_PASS, 0x5555
@@ -11,7 +11,7 @@ _start:
   # set s1 to the output address
   li s1, UART_BASE_ADDR
   # set s2 to the input address
-  la s2, message - 1    # s2 := <message>
+  la s2, message - 1
   # s3 is the sum counter, set to zero
   addi s3, x0, 0
 
@@ -27,9 +27,9 @@ loop_lines:
   
   loop_chars:
     # add one to the input address (initial value is -1)
-    addi s2, s2, 1    # s2 := s2 + 1
+    addi s2, s2, 1
     # load byte to s4 from the address in s2 (the input address)
-    lb s4, 0(s2)      # s4 := (s2)
+    lb s4, 0(s2)
     # finish processing if at the end of input
     beq s4, x0, end_of_input
     # finish processing if at the end of line
@@ -37,6 +37,7 @@ loop_lines:
     # special handling for letters
     bge s4, s10, handle_letter
 
+    # handle number
     # update last and first number variables
     addi t2, s4, 0
     bne t1, x0, loop_chars
@@ -49,78 +50,66 @@ loop_lines:
     loop_numbers:
       # s5 points to the beginning of the current number part
       # t3 is 0..n counter of how far into the string we have looped
-      addi t3, x0, 0
-      loop_name:
-        # t4 is the charcode from target string
+      # loop until we find a difference
+      addi t3, x0, -1
+      loop_until_different:
+        addi t3, t3, 1
+        # t4 = char from target string
         add t4, t3, s5
         lb t4, 0(t4)
-        # t5 is the charcode from input string
+        # t5 = char from input string
         add t5, t3, s2
         lb t5, 0(t5)
+        beq t4, t5, loop_until_different
 
-        # when they match, move to next char
-        bne t4, t5, no_match
-          addi t3, t3, 1
-          j loop_name
-        no_match:
-
-        # when the target ends, we have found the whole name
-        bne t4, x0, incorrect
-          # load the numeric value and update t1/t2
-          add t4, t3, s5
-          lb t4, 1(t4)
-          addi t2, t4, 0
-          bne t1, x0, end_of_number
-          addi t1, t4, 0
-          j end_of_number
-
-        # target and input were different, just move to next number candidate
-        incorrect:
-          j end_of_number
-
+      # when the target ends, we have found the whole name
+      bne t4, x0, end_of_number
+        # load the numeric value and update t1/t2
+        add t4, t3, s5
+        lb t4, 1(t4)
+        addi t2, t4, 0
+        bne t1, x0, end_of_number
+        addi t1, t4, 0
+      # otherwise target and input were different,
+      # just move to next number candidate
       end_of_number:
         # here we can assume that
         #  s5 points to the beginning of the current number part
         #  t3 is a 0..n counter somewhere in the current number, might be at the zero char at the end
-        # move forward until 0x00 char, t4 will be the char after
-        add t4, s5, t3
+        # move forward until the zero char if not already there
+        add s5, s5, t3
         loop_until_at_end:
-          lb t5, 0(t4)
-          addi t4, t4, 1
+          lb t5, 0(s5)
+          addi s5, s5, 1
           bne t5, x0, loop_until_at_end
-        # now t4 is pointer to the value char after the string
+        # now s5 is pointer to the value char after the string
         # set s5 to the start of the next number section
-        addi s5, t4, 1
-        # check if the next sections starts with \n instead of a letter char
+        addi s5, s5, 1
+        # check if the next section starts with zero-char instead of a letter char
+        # then we are at the end of the numbers to find
         lb t4, 0(s5)
-        bne t4, s11, loop_numbers
-
-    j loop_chars
+        beq t4, x0, loop_chars
+        j loop_numbers
 
   end_of_line:
-    # debug: print calibration value for the line
+    # debug: print calibration value for each line
     sb t1, 0(s1)
     sb t2, 0(s1)
     sb s11, 0(s1)
-    
     # convert chars to numbers
     addi t1, t1, -48 # 48 = '0'
     addi t2, t2, -48
-
-    # t3 = 10 * t1 + t2
+    # t3 = 10t1 + t2
     mul t1, t1, s11
     add t3, t1, t2
-
     # add to the sum
     add s3, s3, t3
     j loop_lines
 
+# Print the result by calculating it into "stack" backwards first
 end_of_input:
-  # Print the result by calculating it into "stack" backwards first
-
   la t2, stack
   addi s2, t2, 0
-
   inputloop:
     remu t1, s3, s11
     addi t1, t1, 48
@@ -128,13 +117,11 @@ end_of_input:
     addi s2, s2, 1
     divu s3, s3, s11
     bne  s3, x0, inputloop
-
   outputloop:
     addi s2, s2, -1
     lb   t1, 0(s2)
     sb   t1, 0(s1)
     bne  s2, t2, outputloop
-
   sb s11, 0(s1)
 
   # exit gracefully
@@ -149,7 +136,7 @@ end_of_input:
 # alignment not necessary, just making it easier to see where the stuff is in memory dump
 .balign 16, 0
 numbers:
-  .string "one", "1two", "2three", "3four", "4five", "5six", "6seven", "7eight", "8nine", "9\n" # line break at the end
+  .string "one", "1two", "2three", "3four", "4five", "5six", "6seven", "7eight", "8nine", "9"
 
 # reserve 16 characters of space to store the number to output in reverse order
 stack:
